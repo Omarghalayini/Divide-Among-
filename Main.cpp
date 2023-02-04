b2666dff271eef9f59c2cd28457ed40fd2e8be0c
Omarghalayini <42498579+Omarghalayini@users.noreply.github.com>
2023-02-04 20:59:42 +0000

    Add files via upload

---
+++ Main.cpp
@@ -0,0 +1,34 @@
+ #include <iostream>
+
+ using namespace std;
+
+ int main(){
+
+int chocolate, children;
+
+cout << " Please enter how many children and how many chocolates there are";
+cin >> chocolate >> children;
+
+
+cout <<" How many chocolates each child gets: " << chocolate / children << endl;
+
+cout <<"How much chocolates is left over: " << chocolate % children;
+
+
+
+
+ return 0;
+
+ }
+
+
+
+
+
+
+
+
+
+
+
+
